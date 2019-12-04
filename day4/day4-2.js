83a333524776f9cf8b8dbcb61f071de56ae66910
Daniel Trost <dttrost@gmail.com>
2019-12-04 10:14:17 -0700

    Day 4 solutions

---
+++ day4/day4-2.js
@@ -0,0 +1,28 @@
+const fs = require('fs');
+
+const compute = function() {
+  const start = 402328;
+  const end = 864247;
+  let count = 0;
+
+  for (var i = start; i <= end; i++) {
+    const values = String(i).split('');
+    const isIncrementing = values.every(
+      (v, i) => values[i + 1] === undefined || v <= values[i + 1]
+    );
+
+    if (isIncrementing) {
+      const hasTwoOfValue = values.some(
+        v => values.filter(val => val === v).length === 2
+      );
+
+      if (hasTwoOfValue) {
+        count++;
+      }
+    }
+  }
+
+  return count;
+};
+
+console.log(compute());
